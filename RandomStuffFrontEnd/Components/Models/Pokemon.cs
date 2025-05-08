b0e592d37ec06b1e38dfb6cf9bdbbefc85933396
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-05-08 11:22:30 -0700

    Add project files.

---
+++ RandomStuffFrontEnd/Components/Models/Pokemon.cs
@@ -0,0 +1,8 @@
+﻿namespace RandomStuffFrontEnd.Components.Models
+{
+    public class Pokemon
+    {
+        public long PokemonId { get; set; }
+        public string PokemonName { get; set; }
+    }
+}
