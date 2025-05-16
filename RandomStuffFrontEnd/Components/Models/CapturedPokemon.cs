e682095f7e38742afaee6e62aa50bc6682097756
Jorge Hernandez Cruz <jlhc3751@gmail.com>
2025-05-16 13:28:14 -0700

    Created new models, including a LoginUserModel to handle logging in. With the backend allowing FromBody, if your inputs match an existing user inside the system, you will be redirected to the pokedex.
    That will soon change to displaying your own personal pokemon.

---
+++ RandomStuffFrontEnd/Components/Models/CapturedPokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomStuffFrontEnd.Components.Models
+{
+    public class CapturedPokemon
+    {
+        public long CapturedPokemonId { get; set; }
+        public string Nickname { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; }
+        public long PokemonId { get; set; }
+        public Pokemon Pokemon { get; set; }
+    }
+}
